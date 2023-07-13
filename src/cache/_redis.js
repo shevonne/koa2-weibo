ab215bf4381b17605f22c218302e889fe3b967f2
shevonne <zxf199212146@163.com>
2023-07-13 22:11:17 +0800

    redis get set 方法完善

--- src/cache/_redis.js
+++ src/cache/_redis.js
@@ -9,10 +9,58 @@ const {REDIS_CONF} = require("./../conf/db");
 //创建客户端
 const redisClient = redis.createClient(REDIS_CONF.port,REDIS_CONF.host);
 
+//监听错误
 redisClient.on("error", err => {
    console.log("error",err)
 })
 
-//get
 
-//set
+/**
+ * redis set 
+ * @param {string} key 键
+ * @param {string} val 值
+ * @param {number} timout 过期时间
+ */
+function set(key,val,timout = 60 * 60){
+    //对象转字符串存储
+    if(typeof(val) === "object"){
+        val = JSON.stringify(val);
+    }
+    //设置值
+    redisClient.set(key,val);
+    //过期时间
+    redisClient.expire(key,timout)
+}
+
+
+/**
+ * redis get
+ * @param {string} key 
+ * 
+ */
+function get(key){
+    const promise = new Promise((resolve,reject) => {
+        redisClient.get(key,(err,val)=>{
+            if(err){
+                reject(err)
+                return
+            }
+            if(val == null){
+                resolve(null)
+                return
+            }
+            //取值
+            try {
+                resolve(JSON.parse(val))
+            } catch (error) {
+                resolve(val)
+            }
+        })
+    })
+    return promise;
+}
+
+module.exports = {
+    get,
+    set
+}
